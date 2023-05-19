bdf059f765585cb252c2bb21f4de598db6260a25
jangel01 <jasonangel@csu.fullerton.edu>
2023-05-19 13:53:01 -0700

    clean up code

--- routes.py
+++ routes.py
@@ -11,76 +11,55 @@ app_router = APIRouter()
 def index():
 	return {"book": "Root page"}
 
+# get all books
 @app_router.get("/books")
 async def get_todos():
     todos = todos_serializer(collection.find())
-    #return {"status": "ok", "data": todos}
     return {"data": todos}
 
+# get book by id
 @app_router.get("/books/{book_id}")
 async def get_todos_id(book_id:str=Path(...,min_length=24, max_length=24)):
-    #64370af1915d810bfbe7a08e - not valid book
-    #6452df1b790b5f84a6061c74 - valid book
-    #db.catalog.find({ _id: "64370af1915d810bfbe7a08d"})
     todos = todos_serializer(collection.find({ "_id": ObjectId(book_id)}))
     return {"Results": todos}
 
+# get total stock of books
 @app_router.get("/inventory")
 async def get_todos_inventory():
-    #mongosh query - total titles
-    #db.catalog.aggregate( [ { $match: { sales: { $gte: 0 } } }, { $count: "total books" } ] )
-    #db.catalog.aggregate([{ "$group": { "_id": "$None", "total collections": { "$sum": 1 } } }])
-    #pipeline = [ { "$match": { "sales": { "$gte": 0 } } }, { "$count": "total books" } ]
-    #pipeline = ([{"$group": {"_id":"$None", "total collections":{"$sum": 1}}}])
-    #totalBooks = collection.aggregate( pipeline )
-
-    #mongosh query - total books
-    #db.catalog.aggregate([{ $group: {_id: null, sumOfBooks: {$sum: "$stock"} }}])
     pipeline = [{ "$group": {"_id": None, "sumOfBooks": {"$sum": "$stock"} }}]
 
     totalBooks = collection.aggregate( pipeline )
-    #return list(totalBooks)
     return list(totalBooks)
 
+# get top 5 best seller books
 @app_router.get("/bestSeller", response_class=HTMLResponse)
 async def get_todos_best_seller():
-
-    #mongosh query for best seller
-    #db.catalog.aggregate([{"$group": {"_id": "$sales", "titles": {"$push": "$title"}}}, {"$sort": {"_id" : -1}}, {"$limit" : 5}])
-    #db.catalog.aggregate([{"$group": {"_id": "$title", "author": {"$push": "$author"}, "sales": {"$push": "$sales"}}}, {"$sort": {"sales" : -1}}, {"$limit" : 5}])
-    #db.catalog.aggregate( [{"$unwind": "$title" }, { $group: { _id: "$sales", title: { $topN: { output: ["$title", "$sales"], sortBy: { "sale": -1 }, n : 5 } } } }, {"$sort": {"_id" : -1}}, {"$limit" : 5}])
-    
-    #pipeline = [{"$group": {"_id": "$sales", "title": {"$push": "$title"}}}, {"$sort": {"_id" : -1}}, {"$limit" : 5}]
     pipeline = [{"$group": {"_id": "$title", "author": {"$push": "$author"}, "sales": {"$push": "$sales"}}}, {"$sort": {"sales" : -1}}, {"$limit" : 5}]
     bestSeller = collection.aggregate(pipeline)
     bestList = ""
     x = 1
     for item in bestSeller:
-        #print(item)
         bestList = bestList + str(item)
         if (x == 5): break
         else: x = x + 1
 
     return bestList
 
+# get top 5 authors with most books
 @app_router.get("/topAuthors", response_class=HTMLResponse)
 async def get_todos_top_author():
-        #mongosh query for best seller
-    #db.catalog.aggregate([{'$group': {'_id': '$author', 'countStock': {'$sum': "$stock"}}},{'$sort': {'countStock': -1}}, {"$limit" : 5}])
-    
     pipeline = [{'$group': {'_id': '$author', 'countStock': {'$sum': "$stock"}}},{'$sort': {'countStock': -1}}, {"$limit" : 5}]
     topAuthors = collection.aggregate(pipeline)
     topList = ""
     x = 1
     for item in topAuthors:
-        #print(item)
         topList = topList + str(item)
-       
         if (x == 5): break
         else:  x = x +1
 
     return topList
 
+# insert book
 @app_router.post("/books")
 async def insert_book(todo: Todo):
     todo = todo.dict()
@@ -88,6 +67,7 @@ async def insert_book(todo: Todo):
     todo = todo_serializer(collection.find_one({"_id": todo.inserted_id}))
     return {"data": todo}
 
+# update book by id
 @app_router.put("/books/{book_id}")
 async def update_book(todo: Todo, book_id:str=Path(...,min_length=24, max_length=24)):
     todo = todo.dict()
@@ -97,6 +77,7 @@ async def update_book(todo: Todo, book_id:str=Path(...,min_length=24, max_length
     todo = todo_serializer(todo)
     return {"data": todo}
 
+# delete book by id
 @app_router.delete("/books/{book_id}")
 async def delete_book(book_id:str=Path(...,min_length=24, max_length=24)):
     todo = collection.find_one({"_id": ObjectId(book_id)})
@@ -105,6 +86,7 @@ async def delete_book(book_id:str=Path(...,min_length=24, max_length=24)):
     collection.delete_one({"_id": ObjectId(book_id)})
     return {"data": todo}
 
+# search book by title, author, minPrice, maxPrice
 @app_router.get("/search")
 async def search_book(title: str, author: str, minPrice: float, maxPrice: float):
     todo = todos_serializer(collection.find({"title": title, "author": author, "price": {"$gte": minPrice, "$lte": maxPrice}}))
